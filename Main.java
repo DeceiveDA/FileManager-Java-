8407218573f2e40e7e87dffc623bba3b19360404
DeceiveDA <39352545+DeceiveDA@users.noreply.github.com>
2020-10-07 10:08:41 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    ButtonBar buttonBar = new ButtonBar();
+
+    Button openButton = new Button("Open File");
+    Button findButton = new Button("Find File");
+    Button createButton = new Button("Create File");
+    Button closeButton = new Button("Close Application");
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("File Manager");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
