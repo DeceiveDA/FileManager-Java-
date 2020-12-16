ad24a9cddece1ee0ab7bba827bc3a48015dd2e67
DeceiveDA <39352545+DeceiveDA@users.noreply.github.com>
2020-12-15 22:49:49 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,92 +1,26 @@
-//Nathaniel Marcum
-//10/14/20
-/*This program is the functionality extension to the FileManager project *C:\Users\BitchCity\IdeaProjcts\*
+package sample;
 
-*/
-package com.company;
-import java.io.*;
-import java.util.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) throws IOException {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button findButton = new Button("Find File");
+        Scene scene = new Scene(findButton, 50, 100);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        //fileContent: User enters desired file name to create if search == NULL.
-        Scanner fileContent = new Scanner(System.in);
 
-        //menuInput: User enters desired action upon entering startup menu
-        Scanner menuInput = new Scanner(System.in);
-
-        //Reads entered Scanner
-        String fileText = fileContent.nextLine();
-        File file = new File(fileText);
-        InputStreamReader cin = null;
-
-        //Creates a new file instance
-        file.createNewFile();
-
-        //Creates a new fileWriter
-        FileWriter fileWriter = new FileWriter(file);
-
-        //Creates file if search is NULL.
-        //
-        //fileWriter.write();
-        fileWriter.flush();
-        fileWriter.close();
-
-        //Creating FileReader Object
-        FileReader fileReader = new FileReader(file);
-
-        //Creates an array for testing
-        char [] a = new char[50];
-        //fileReader.read(a);
-
-        System.out.println("What action would you like to perform?");
-        System.out.println("(O)pen File\t(S)earch File\t(C)reate File\n(D)elete File\t(E)xit Program");
-        menuInput.nextLine();
-
-        //Finish functionality
-        switch(menuInput.toString()) {
-            case "O":
-                //break;
-            case "S":
-                //break;
-            case "C":
-                //break;
-            case "D":
-                //break;
-            case "E":
-                //break
-
-            default:
-                System.out.println("Please enter one of the following options:");
-                break;
-        }
-
-        /*
-        if (menuInput != null) throw InputMismatchException {
-            System.out.println("Please enter one of the following options");
-        }
-        if (menuInput )
-        */
-
-        //If statement to check if fileText is NULL, if NULL or not found throw IO Exception.
-        /*
-        if (fileText == NULL) throwException {
-            System.out.println("Value has come back NULL\nPlease try again...");
-            }
-        if {fileText !
-
-        }
-
-         */
-        for (char c : a) {
-            System.out.print(c);
-        fileReader.close();
-        }
-        /*try {
-            cin = new InputStreamReader(System.in);
-            System.out.prinln("Enter a charactrer")
-        }*/
+    public static void main(String[] args) {
+        launch(args);
     }
 }
