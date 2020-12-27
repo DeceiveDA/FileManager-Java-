470fb26c267c93396061c6fa19bc55a138d5af75
DeceiveDA <39352545+DeceiveDA@users.noreply.github.com>
2020-12-27 01:38:33 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,73 @@
-package sample;
+//Nathaniel Marcum
+//10/14/20
+/*This program is the functionality extension to the FileManager project *C:\Users\BitchCity\IdeaProjcts\*
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+*/
 
-public class Main extends Application {
+package com.company;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Button findButton = new Button("Find File");
-        Scene scene = new Scene(findButton, 50, 100);
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setScene(scene);
-        primaryStage.show();
+import java.io.*;
+import java.util.*;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // String for fileContent
+        String s = "";
+        //fileContent: User enters desired file name to create if search == NULL.
+        Scanner fileContent = new Scanner(s);
+        fileContent.close();
+
+
+        /*Reads entered Scanner
+
+
+        System.out.println("(O)pen File\t(S)earch File\t(C)reate File\n(D)elete File\t(E)xit Program");
+        System.out.println("What action would you like to perform?");
+
+        //menuInput: User enters desired action upon entering startup menu
+        Scanner menuInput = new Scanner(System.in);
+        menuInput.nextLine();
+
+        if(menuInput.) {
+            return
+        }
+
+
+        // Switch case statement for file menu options
+
+
+        //Leave for testing reasons
+        /*for (char c : a) {
+            System.out.print(c);
+        fileReader.close();
+        }*/
     }
 
+    public static void FileHandler(String fileContent, File file) throws IOException, SecurityException {
 
-    public static void main(String[] args) {
-        launch(args);
+        File[] fileList = file.listFiles();
+        System.out.println("What file would you like to search for?");
+
+        /*if (findFile(file) == ) {
+            try {
+                if (fileName == null) {
+                    System.out.println("Please enter a valid file/location");
+                    file.isFile();
+                    //findFile(fileName);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                e.printStackTrace();
+            }
+        }*/
+
+    }
+
+    public String toString() {
+
     }
 }
